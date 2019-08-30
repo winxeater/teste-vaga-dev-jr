cc8c06766bd3f4a2be291a734927c788e42367e5
Matheus <matheus.a.aguiar@hotmail.com>
2019-08-30 13:01:24 -0300

    pagina de cadastro added

--- FirstServer/index.js
+++ FirstServer/index.js
@@ -21,6 +21,10 @@ app.get("/cadastrar", function(req, res){
     res.render('cadastrar')
 });
 
+app.get("/editar/:id", function(req, res){
+    res.render('cadastrar')
+});
+
 app.get("/conteudo", function(req, res){
     Concorrentes.findAll().then(function(posts){
        res.render('conteudo', {posts: posts}) 
@@ -28,10 +32,18 @@ app.get("/conteudo", function(req, res){
     // res.render('conteudo')
 });
 
+app.get('/conteudo/:id', function(req,res){
+    Concorrentes.destroy({where: {'id': req.params.id}}).then(function(){
+        res.redirect('back');
+    }).catch(function(erro){
+        res.send("Erro ao deletar!")
+    })
+});
+
 
 //posts ######form de cadastro
 app.post("/cadastrar", function(req, res){
-    Concorrentes.create({
+      Concorrentes.create({
         nome: req.body.inpNome,
         telefone: req.body.inpTelefone,
         email: req.body.inpEmail,
@@ -40,12 +52,35 @@ app.post("/cadastrar", function(req, res){
         pretSalarial: req.body.inpSalarial,
         habilidades: req.body.inpHabilidades,
         // especial: req.body.radioMarcar
-    }).then(function(){
-        res.redirect('conteudo')
+        }).then(function(){
+            res.redirect('conteudo')
+        }).catch(function(erro){
+            res.redirect('cadastrar')
+        })  
+    
+});
+
+//update cadastro
+app.post('/editar/:id', function(req,res){
+    Concorrentes.update({
+        nome: 'teste de update',
+        telefone: req.body.inpTelefone,
+        email: req.body.inpEmail,
+        nascimento: req.body.inpNascimento,
+        cursFacaul: req.body.radioFacul,
+        pretSalarial: req.body.inpSalarial,
+        habilidades: req.body.inpHabilidades
+        },
+        // especial: req.body.radioMarcar
+        {where: {'id': req.params.id}}
+    ).then(function(){
+        res.redirect('back');
     }).catch(function(erro){
-        res.redirect('cadastrar')
+        res.send("Erro ao atualizar!")
     })
 });
+
+
 
 app.listen(8081, function(){
     console.log("Servidor rodando perfeitamente!")
